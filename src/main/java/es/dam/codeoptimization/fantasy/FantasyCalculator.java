4e31b8dae040841083920802c7acf9dce7880bb4
Joel-Estraviz <joelestraviznogueiro@gmail.com>
2026-04-20 10:31:52 +0200

    renombrarVariables

--- src/main/java/es/dam/codeoptimization/fantasy/FantasyCalculator.java
+++ src/main/java/es/dam/codeoptimization/fantasy/FantasyCalculator.java
@@ -7,7 +7,7 @@ import es.dam.codeoptimization.PlayerStats;
 
 /**
  * THE CLASS YOU HAVE TO MODIFY
- * @author Your Name
+ * @author Joel Estraviz Nogueiro
  */
 public class FantasyCalculator {
 
@@ -16,124 +16,124 @@ public class FantasyCalculator {
         int r = 0; 
         int punts = 0;
         
-        int m = s.minutes;
-        int g = s.goals;
-        int a = s.assists;
-        boolean y = s.yellowCard;
-        boolean red = s.redCard;
-        int p = s.saves;
-        int ga = s.goalsAgainst;
-        char res = s.matchResult;
-        String pos = s.position;
+        int minutes = s.minutes;
+        int goals = s.goals;
+        int assists = s.assists;
+        boolean yeloowCard = s.yellowCard;
+        boolean redCard = s.redCard;
+        int paradas = s.saves;
+        int goalsAgainst = s.goalsAgainst;
+        char matchResult = s.matchResult;
+        String position = s.position;
 
         // --- GOALKEEPER LOGIC ---
-        if (pos.equals("PORTERO")) {
-            if (m > 0 && m < 60) {
+        if (position.equals("PORTERO")) {
+            if (minutes > 0 && minutes < 60) {
                 r = r + 3;
-            } else if (m >= 60) {
+            } else if (minutes >= 60) {
                 r = r + 5;
             }
 
-            for (int i = 0; i < g; i++) {
+            for (int i = 0; i < goals; i++) {
                 r = r + 5;
             }
 
-            r = r + (a * 6);
+            r = r + (assists * 6);
 
             // 1 point per save
-            r = r + p; 
+            r = r + paradas; 
             
-            if (ga == 0) {
+            if (goalsAgainst == 0) {
                 r = r + 5; 
-            } else if (ga == 1) {
+            } else if (goalsAgainst == 1) {
                 r = r + 3;
-            } else if (ga == 2) {
+            } else if (goalsAgainst == 2) {
                 r = r + 1;
             }
 
-            if (y == true) r = r - 3; 
-            if (red == true) r = r - 5;
+            if (yeloowCard == true) r = r - 3; 
+            if (redCard == true) r = r - 5;
             
-            if (res == 'G') {
+            if (matchResult == 'G') {
                 r = r + 5;
-            } else if (res == 'E') {
+            } else if (matchResult == 'E') {
                 r = r + 2;
             }
 
         // --- DEFENDER LOGIC ---
-        } else if (pos.equals("DEFENSA")) {
-            if (m > 0 && m < 60) {
+        } else if (position.equals("DEFENSA")) {
+            if (minutes > 0 && minutes < 60) {
                 r = r + 3;
-            } else if (m >= 60) {
+            } else if (minutes >= 60) {
                 r = r + 5;
             }
 
-            for (int i = 0; i < g; i++) {
+            for (int i = 0; i < goals; i++) {
                 r = r + 5;
             }
 
-            r = r + (a * 6);
+            r = r + (assists * 6);
 
-            if (ga == 0) {
+            if (goalsAgainst == 0) {
                 r = r + 5; 
-            } else if (ga == 1) {
+            } else if (goalsAgainst == 1) {
                 r = r + 3;
-            } else if (ga == 2) {
+            } else if (goalsAgainst == 2) {
                 r = r + 1;
             }
 
-            if (y == true) r = r - 3;
-            if (red == true) r = r - 5;
+            if (yeloowCard == true) r = r - 3;
+            if (redCard == true) r = r - 5;
             
-            if (res == 'G') {
+            if (matchResult == 'G') {
                 r = r + 5;
-            } else if (res == 'E') {
+            } else if (matchResult == 'E') {
                 r = r + 2;
             }
 
         // --- MIDFIELDER LOGIC ---
-        } else if (pos.equals("MEDIO")) {
-            if (m > 0 && m < 60) {
+        } else if (position.equals("MEDIO")) {
+            if (minutes > 0 && minutes < 60) {
                 r = r + 3;
-            } else if (m >= 60) {
+            } else if (minutes >= 60) {
                 r = r + 5;
             }
 
-            for (int i = 0; i < g; i++) {
+            for (int i = 0; i < goals; i++) {
                 r = r + 5;
             }
 
-            r = r + (a * 6);
+            r = r + (assists * 6);
 
-            if (y == true) r = r - 3;
-            if (red == true) r = r - 5;
+            if (yeloowCard == true) r = r - 3;
+            if (redCard == true) r = r - 5;
             
-            if (res == 'G') {
+            if (matchResult == 'G') {
                 r = r + 5;
-            } else if (res == 'E') {
+            } else if (matchResult == 'E') {
                 r = r + 2;
             }
 
         // --- FORWARD LOGIC ---
-        } else if (pos.equals("DELANTERO")) {
-            if (m > 0 && m < 60) {
+        } else if (position.equals("DELANTERO")) {
+            if (minutes > 0 && minutes < 60) {
                 r = r + 3;
-            } else if (m >= 60) {
+            } else if (minutes >= 60) {
                 r = r + 5;
             }
 
-            for (int i = 0; i < g; i++) {
+            for (int i = 0; i < goals; i++) {
                 r = r + 6;
             }
 
-            r = r + (a * 5);
+            r = r + (assists * 5);
 
-            if (y == true) r = r - 3;
-            if (red == true) r = r - 5;
+            if (yeloowCard == true) r = r - 3;
+            if (redCard == true) r = r - 5;
             
-            if (res == 'G') {
+            if (matchResult == 'G') {
                 r = r + 5;
-            } else if (res == 'E') {
+            } else if (matchResult == 'E') {
                 r = r + 2;
             }
         }
